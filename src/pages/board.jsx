e6aa2da56ae46604247a3804139e60c7efb3c852
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-08-29 15:15:21 +0700

    backspace and clear done

--- src/pages/board.jsx
+++ src/pages/board.jsx
@@ -1,63 +1,65 @@
-import React, { useState } from 'react'
-import './style.css'
+import React, { useRef, useState } from "react";
+import "./style.css";
 
 const Board = () => {
-
-  const [counter, setCounter] = useState('');
+  const [counter, setCounter] = useState("");
   const [result, setResult] = useState(0);
+  const storage = useRef(null);
 
   function angka(params) {
-   setCounter(counter + params);
+    setCounter(counter + params);
   }
 
-  function handleTambah() {
-    setResult(Number(counter) + result);
-    counter = '';
-  }
-
-  function handleKurang() {
-    setResult(Number(counter) - result)
-  }
-
-  function handleKali() {
-    setResult(Number(counter) * result)
-  }
-
-  function handleBagi() {
-    setResult(Number(counter) / result)
+  function handleAction(action) {
+    if (action === 'b') {
+      const eraseNumber = counter.split('');
+      eraseNumber.pop();
+      setCounter(eraseNumber.join(''));
+    }
+    if (action === 'c') {
+      storage.current = null;
+      setCounter('')
+      setResult(0)
+    }
   }
 
 
   return (
     <>
-      <div className='board-calculating'>
+      <div className="board-calculating">
         <div>Board</div>
-        <div className='board-result'>Ini buat Hasil : {counter}</div>
-        <button>C</button>
-        <button onClick={handleBagi}>:</button>
-        <button onClick={handleKali}>x</button>
-        <button>Backspace</button>
-        <br />
-        <button onClick={()=> {angka('7')}}>7</button>
-        <button onClick={()=> {angka('8')}}>8</button>
-        <button onClick={()=> {angka('9')}}>9</button>
-        <button onClick={handleKurang}>-</button>
-        <br />
-        <button onClick={()=> {angka('4')}}>4</button>
-        <button onClick={()=> {angka('5')}}>5</button>
-        <button onClick={()=> {angka('6')}}>6</button>
-        <button onClick={handleTambah}>+</button>
-        <br />
-        <button onClick={()=> {angka('1')}}>1</button>
-        <button onClick={()=> {angka('2')}}>2</button>
-        <button onClick={()=> {angka('3')}}>3</button>
-        <button>=</button>
-        <br />
-        <button onClick={()=> {angka('0')}}>0</button>
-        <button onClick={() => {angka('.')}}>.</button>
+        <div className="board-result">Ini buat Hasil : {!counter ? result : counter}</div>
+        <div>
+          <button onClick={() => handleAction('c')}>C</button>
+          <button onClick={() => handleAction(':')}>:</button>
+          <button onClick={() => handleAction('x')}>x</button>
+          <button onClick={() => handleAction('b')}>Backspace</button>
+        </div>
+        <div>
+          <button onClick={() => {angka("7")}}>7</button>
+          <button onClick={() => {angka("8")}}>8</button>
+          <button onClick={() => {angka("9")}}>9</button>
+          <button onClick={()=> handleAction('-')}>-</button>
+        </div>
+        <div>
+          <button onClick={() => {angka("4")}}>4</button>
+          <button onClick={() => {angka("5")}}>5</button>
+          <button onClick={() => {angka("6")}}>6</button>
+          <button onClick={() => handleAction('+')}>+</button>
+        </div>
+        <div>
+          <button onClick={() => {angka("1");}}>1</button>
+          <button onClick={() => {angka("2");}}>2</button>
+          <button onClick={() => {angka("3");}}>3</button>
+          <button onClick={() => handleAction('=')}>=</button>
+        </div>
+        <div>
+          <button onClick={() => {angka("0");}}>0</button>
+          <button onClick={() => {angka(".");}}>.</button>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
